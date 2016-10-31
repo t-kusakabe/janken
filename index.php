3db666fccce353b09274836fbcb9135a227632c9
華一匁 <hana@hana.private>
2016-11-01 00:32:21 +0900

    add index page

--- index.php
+++ index.php
@@ -6,6 +6,28 @@
   </head>
 
   <body>
-
+    <div class="contents">
+      <form action="check.php" method="POST">
+        <div class="content">
+          <label>
+            <img class="hand" src="/images/gu-.png"><br>
+            <input class="icon" type="radio" name="icon">
+          </label>
+        </div>
+        <div class="content">
+          <label>
+            <img class="hand" src="/images/tyoki.png"><br>
+            <input class="icon" type="radio" name="icon">
+          </label>
+        </div>
+        <div class="content">
+          <label>
+            <img class="hand" src="/images/pa-.png"><br>
+            <input class="icon" type="radio" name="icon">
+          </label>
+        </div>
+        <input type="submit" value="じゃんけんぽんっ！">
+      </form>
+    </div>
   </body>
 </html>
