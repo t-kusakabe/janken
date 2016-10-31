f9da3533072423dd130c0649f98ab05d38094e73
華一匁 <hana@hana.private>
2016-11-01 00:49:15 +0900

    playerとcpuの手を出せるように

--- check.php
+++ check.php
@@ -6,6 +6,28 @@
   </head>
 
   <body>
+    <div class="contents">
+      <div class="player">
+        <?php if ($_POST['icon'] == 0) { ?>
+          <img src="/images/gu-.png">
+        <?php } else if($_POST['icon'] == 1) { ?>
+          <img src="/images/tyoki.png">
+        <?php } else { ?>  
+          <img src="/images/pa-.png">
+        <?php } ?>
+      </div>
 
+      <?php $cpu_hand = rand(0, 2); ?>
+
+      <div class="cpu">
+        <?php if ($cpu_hand == 0) { ?>
+          <img src="/images/gu-.png">
+        <?php } else if($cpu_hand == 1) { ?>
+          <img src="/images/tyoki.png">
+        <?php } else { ?>  
+          <img src="/images/pa-.png">
+        <?php } ?>
+      </div>
+    </div>
   </body>
 </html>
