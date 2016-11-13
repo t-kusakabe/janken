5d8e589b5bfa616ba932b92edd8a9ac3cab1ed3f
華一匁 <hana@hana.private>
2016-11-13 12:30:26 +0900

    色々fix

--- check.php
+++ check.php
@@ -2,14 +2,14 @@
 <html>
   <head>
     <title>じゃんけんゲーム</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
   </head>
 
   <body>
     <?php
       if (is_null($_POST['icon'])) {
         $message = '手を選んでください';
-        header("Location: /php/index.php?message={$message}");
+        header("Location: /index.php?message={$message}");
       }
     ?>
 
@@ -22,7 +22,7 @@
       <div id="player">
         <?php if ($_POST['icon'] == 0) { ?>
           <img src="ぐー画像の名前">
-        <?php ここに書いてね ?>
+        <?php ** ここに書いてね **  ?>
           <img src="ちょき画像の名前">
         <?php } else { ?>  
           <img src="ぱー画像の名前">
@@ -30,12 +30,12 @@
         <span>あなた</span>
       </div>
 
-      <?php $箱の名前 = rand(0, 2); ?>
+      <?php ** $箱の名前 **  = rand(0, 2); ?>
 
       <div id="cpu">
-        <?php if ($箱の名前 == 0) { ?>
+        <?php if (** $箱の名前 ** == 0) { ?>
           <img src="/images/gu-.png">
-        <?php } else if($箱の名前 == 1) { ?>
+        <?php } else if(** $箱の名前 ** == 1) { ?>
           <img src="/images/tyoki.png">
         <?php } else { ?>  
           <img src="/images/pa-.png">
@@ -47,13 +47,13 @@
     <div id="result">
       <h1>
         <?php
-          if ($自分の手 == $相手の手) {
+          if (** $自分の手 ** == ** $相手の手 **) {
             echo 'あいこ！';
           } else if 
             (
-              $_POST['icon'] == ぐーかちょきかぱー && $cpu_hand == ぐーかちょきかぱー
-              || $_POST['icon'] == ぐーかちょきかぱー && $cpu_hand == ぐーかちょきかぱー
-              || $_POST['icon'] == ぐーかちょきかぱー && $cpu_hand == ぐーかちょきかぱー
+              $_POST['icon'] == ** ぐーかちょきかぱー ** && $cpu_hand == ** ぐーかちょきかぱー **
+              || $_POST['icon'] == ** ぐーかちょきかぱー ** && $cpu_hand == ** ぐーかちょきかぱー **
+              || $_POST['icon'] == ** ぐーかちょきかぱー ** && $cpu_hand == ** ぐーかちょきかぱー **
             ) 
           {
             echo 'かち！';
@@ -62,7 +62,7 @@
           }
         ?>
       </h1>
-      <a href="/php/index.php">もう一度</a>
+      <a href="/index.php">もう一度</a>
     </div>
   </body>
 </html>
